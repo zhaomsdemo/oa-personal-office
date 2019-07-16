1873c17a24335ba44aa4c4364fd5b3c00f1b5598
zhaomsdemo <zhaomsdemo@gmail.com>
2019-07-15 22:44:22 -0700

    init

---
+++ src/main/java/com/ootb/oa/personaloffice/PersonalofficeApplication.java
@@ -0,0 +1,13 @@
+package com.ootb.oa.personaloffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalofficeApplication.class, args);
+    }
+
+}
